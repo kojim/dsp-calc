2cdf59530be607b11ca0c07428be78c71c61b7af
ykojim <ykojim@gmail.com>
2021-02-20 20:38:37 +0900

    initial commit

---
+++ js/core.js
@@ -0,0 +1,429 @@
+// -------------------------------------------
+// クラス定義
+// -------------------------------------------
+// 生産物用抽象クラス
+var Product = function(){};
+Product.prototype = {
+	/*
+	 * 生産物を秒間 product_per_sec個作るのに必要な施設数
+	 */
+	require_builder_count: function(product_per_sec) {
+		return product_per_sec / ((1/this.req_time) * this.result_count * this.build_rate);
+	},
+	/*
+	 * 生産物を秒間 product_per_sec個作るのに必要な素材と量
+	 * import_ingredientsに含まれる素材は計上しない
+	 */
+	require_ingredient_count: function(product_per_sec, import_ingredients) {
+		var thiz = this;
+		// 各素材の生産量を計算
+		// map   : [{鉄:毎秒1}, {銅:毎秒2, 鉄:毎秒1}], {}] のようなリストを作る。
+		// reduce: {鉄:毎秒3, 銅:毎秒2} ハッシュを作る。
+		var result = this.ingredients.map(function(i) {
+			var ingredient       = i[0]
+			var ingredient_count = i[1];
+			// 計算式:
+			//   施設1つあたりの必要素材数
+			//   / ビルド時間
+			//   * 施設数
+			var build_time     = thiz.req_time/thiz.build_rate;
+			var ingredient_cps = ingredient_count / build_time * thiz.require_builder_count(product_per_sec);
+			var result         = ingredient.require_ingredient_count(ingredient_cps, import_ingredients);
+			return result;
+		}).reduce(function(pre, cur) {
+			$.each(cur, function(ingredient_name, ingredient_count) {
+				if (pre[ingredient_name] === undefined) {
+					// 値がまだないとき
+					pre[ingredient_name] = 0;
+				}
+				pre[ingredient_name] += ingredient_count;
+			});
+			return pre;
+		}, {});
+		// 素材が搬入品に含まれていれば素材の素材の生産量は0に
+		if ($.inArray(this.name, import_ingredients) !== -1) {
+			$.each(Object.keys(result), function(i, name) {
+				result[name] = 0;
+			});
+		}
+		// 自分自身の生産量を付け足す
+		result[this.name] = product_per_sec;
+		return result;
+	}
+};
+
+// 組立機で作るもの
+var AssemblyProduct = function(name, name_jp, req_time, result_count, ingredients) {
+	this.name         = name;
+	this.name_jp      = name_jp;
+	this.req_time     = req_time;
+	this.result_count = result_count;
+	this.ingredients  = ingredients;
+};
+AssemblyProduct.prototype = new Product();
+AssemblyProduct.prototype.build_rate = 0.75;
+AssemblyProduct.prototype.energy_usage = 150;
+
+// 化学プラントで作るもの
+var ChemicalProduct = function(name, name_jp, req_time, result_count, ingredients) {
+	this.name         = name;
+	this.name_jp      = name_jp;
+	this.req_time     = req_time;
+	this.result_count = result_count;
+	this.ingredients  = ingredients;
+};
+ChemicalProduct.prototype = new Product();
+ChemicalProduct.prototype.build_rate = 1.25;
+ChemicalProduct.prototype.energy_usage = 210;
+
+// 原油採掘
+var PumpJack = function(name, name_jp, req_time, result_count, ingredients) {
+	this.name         = name;
+	this.name_jp      = name_jp;
+	this.req_time     = req_time;
+	this.result_count = result_count;
+	this.ingredients  = ingredients;
+};
+PumpJack.prototype = new Product();
+PumpJack.prototype.build_rate = 1;
+PumpJack.prototype.energy_usage = 90;
+
+// 原油精製
+var OilRefinery = function(name, name_jp, req_time, result_count, ingredients) {
+	this.name         = name;
+	this.name_jp      = name_jp;
+	this.req_time     = req_time;
+	this.result_count = result_count;
+	this.ingredients  = ingredients;
+};
+OilRefinery.prototype = new Product();
+OilRefinery.prototype.build_rate = 1;
+OilRefinery.prototype.energy_usage = 420;
+
+// 採掘機で掘り出すもの
+var Ore = function(name, name_jp, hardness, req_time) {
+	this.name     = name;
+	this.name_jp      = name_jp;
+	this.hardness = hardness;
+	this.req_time = req_time;
+};
+Ore.prototype = new Product();
+Ore.prototype.ingredients = [];
+Ore.prototype.energy_usage = 90;
+Ore.prototype.require_builder_count =  function(product_per_sec) {
+	var power = 3;
+	var speed = 0.5;
+	return product_per_sec / (((power - this.hardness) * speed) / this.req_time);
+};
+
+// 溶鉱炉で作るもの
+var MetalPlate = function(name, name_jp, req_time, result_count, ingredients) {
+	this.name         = name;
+	this.name_jp      = name_jp;
+	this.req_time     = req_time;
+	this.result_count = result_count;
+	this.ingredients  = ingredients;
+};
+MetalPlate.prototype = new Product();
+MetalPlate.prototype.build_rate = 2;
+MetalPlate.prototype.energy_usage = 180;
+
+
+// -------------------------------------------
+// データ定義
+// -------------------------------------------
+// -----------------
+// 鉱石
+// -----------------
+var iron_ore = new Ore(
+  "IronOre", "鉄鉱石", 0.9, 2
+);
+var copper_ore = new Ore(
+  "CopperOre", "銅鉱石", 0.9, 2
+);
+var coal = new Ore(
+  "Coal", "石炭", 0.9, 2
+);
+var stone = new Ore(
+  "Stone", "石", 0.4, 2
+);
+
+// -----------------
+// 精錬系
+// -----------------
+var iron_plate = new MetalPlate(
+  "IronPlate", "鉄板", 3.5, 1, [[iron_ore, 1]]
+);
+var copper_plate = new MetalPlate(
+  "CopperPlate", "銅板", 3.5, 1, [[copper_ore, 1]]
+);
+var stone_brick = new MetalPlate(
+  "StoneBrick", "石レンガ", 3.5, 1, [[stone, 2]]
+);
+var steel_plate = new MetalPlate(
+  "SteelPlate", "鋼材", 17.5, 1, [[iron_plate, 5]]
+);
+var concrete = new AssemblyProduct(
+  "Concrete", "コンクリート", 10, 10, [[stone_brick, 5], [iron_ore, 1]]
+);
+
+// -----------------
+// 原油系
+// -----------------
+var crude_oil = new PumpJack(
+  "CrudeOil", "原油", 1, 5, []
+);
+var petroleum_gas = new OilRefinery(
+  "PetroleumGas", "プロパンガス", 5, 5.5, [[crude_oil, 10]]
+);
+var light_oil = new OilRefinery(
+  "LightOil", "軽油", 5, 4.5, [[crude_oil, 10]]
+);
+var heavy_oil = new OilRefinery(
+  "HeavyOil", "重油", 5, 1, [[crude_oil, 10]]
+);
+var lubricant = new ChemicalProduct(
+  "Lubricant", "潤滑油", 1, 1, [[heavy_oil, 1]]
+);
+
+// -----------------
+// 化学系
+// -----------------
+var plastic_bar = new ChemicalProduct(
+  "PlasticBar", "プラスチック棒", 1, 2, [[coal, 1], [petroleum_gas, 3]]
+);
+var sulfur = new ChemicalProduct(
+  "Sulfur", "硫黄", 1, 2, [[petroleum_gas, 3]]
+);
+var sulfuric_acid = new ChemicalProduct(
+  "SulfuricAcid", "硫酸", 1, 5, [[sulfur, 2], [iron_plate, 1]]
+);
+var explosives = new ChemicalProduct(
+  "Explosives", "火薬", 5, 1, [[sulfur, 1], [coal, 1]]
+);
+
+// -----------------
+// 中間素材
+// -----------------
+var iron_gear_wheel = new AssemblyProduct(
+  "IronGearWheel", "歯車", 0.5, 1, [[iron_plate, 2]]
+);
+var copper_cable = new AssemblyProduct(
+  "CopperCable", "銅線", 0.5, 2, [[copper_plate, 1]]
+);
+var pipe = new AssemblyProduct(
+  "Pipe", "パイプ", 0.5, 1, [[iron_plate, 1]]
+);
+var electronic_circuit = new AssemblyProduct(
+  "ElectronicCircuit", "電子回路", 0.5, 1, [[iron_plate, 1], [copper_cable, 3]]
+);
+var advanced_circuit = new AssemblyProduct(
+  "AdvancedCircuit", "発展回路", 8, 1, [[electronic_circuit, 2], [plastic_bar, 2], [copper_cable, 4]]
+);
+var processing_unit = new AssemblyProduct(
+  "ProcessingUnit", "制御基板", 15, 1, [[electronic_circuit, 20], [advanced_circuit, 2], [sulfuric_acid, 0.5]]
+);
+var electronic_drill = new AssemblyProduct(
+  "ElectronicDrill", "電動ドリル", 2, 1, [[steel_plate, 10], [iron_gear_wheel, 5], [electronic_circuit, 3]]
+);
+
+// -----------------
+// 物流系
+// -----------------
+var basic_transport_belt = new AssemblyProduct(
+  "BasicTransportBelt", "ベルトコンベア", 0.5, 2, [[iron_plate, 1], [iron_gear_wheel, 1]]
+);
+var fast_transport_belt = new AssemblyProduct(
+  "FastTransportBelt", "高速ベルトコンベア", 0.5, 1, [[basic_transport_belt, 1], [iron_gear_wheel, 5]]
+);
+var express_transport_belt = new AssemblyProduct(
+  "ExpressTransportBelt", "超高速ベルトコンベア", 0.5, 1, [[fast_transport_belt, 1], [iron_gear_wheel, 5], [lubricant, 2]]
+);
+var basic_underground_belt = new AssemblyProduct(
+  "BasicUndergroundBelt", "地下輸送ベルト", 1, 2, [[iron_plate, 10], [basic_transport_belt, 5]]
+);
+var fast_underground_belt = new AssemblyProduct(
+  "FastUndergroundBelt", "高速地下輸送ベルト", 0.5, 2, [[iron_gear_wheel, 20], [basic_underground_belt, 2]]
+);
+var express_underground_belt = new AssemblyProduct(
+  "ExpressUndergroundBelt", "超高速地下輸送ベルト", 0.5, 2, [[iron_gear_wheel, 40], [fast_underground_belt, 2], [lubricant, 4]]
+);
+var splitter = new AssemblyProduct(
+  "Splitter", "分配器", 1, 1, [[electronic_circuit, 5], [iron_plate, 5], [basic_transport_belt, 4]]
+);
+var fast_splitter = new AssemblyProduct(
+  "FastSplitter", "高速分配器", 2, 1, [[splitter, 1], [iron_gear_wheel, 10], [electronic_circuit, 10]]
+);
+var express_splitter = new AssemblyProduct(
+  "ExpressSplitter", "超高速分配器", 2, 1, [[fast_splitter, 1], [iron_gear_wheel, 10], [advanced_circuit, 10], [lubricant, 8]]
+);
+var basic_inserter = new AssemblyProduct(
+  "BasicInserter", "インサーター", 0.5, 1, [[electronic_circuit, 1], [iron_gear_wheel, 1], [iron_plate, 1]]
+);
+var fast_inserter = new AssemblyProduct(
+  "FastInserter", "高速インサーター", 0.5, 1, [[electronic_circuit, 2], [iron_plate, 2], [basic_inserter, 1]]
+);
+var filter_inserter = new AssemblyProduct(
+  "FilterInserter", "フィルターインサーター", 0.5, 1, [[fast_inserter, 1], [electronic_circuit, 4]]
+);
+var stack_inserter = new AssemblyProduct(
+  "StackInserter", "スタックインサーター", 0.5, 1, [[fast_inserter, 1], [iron_gear_wheel, 15], [electronic_circuit, 15], [advanced_circuit, 1]]
+);
+var stack_filter_inserter = new AssemblyProduct(
+  "StackFilterInserter", "スタックフィルターインサーター", 0.5, 1, [[stack_inserter, 1], [electronic_circuit, 5]]
+);
+
+// -----------------
+// 機械類
+// -----------------
+var battery = new ChemicalProduct(
+  "Battery", "電池", 5, 1, [[sulfuric_acid, 2], [iron_plate, 1], [copper_plate, 1]]
+);
+var engine_unit = new AssemblyProduct(
+  "EngineUnit", "エンジンユニット", 20, 1, [[steel_plate, 1], [iron_gear_wheel, 1], [pipe, 2]]
+);
+var electric_engine_unit = new AssemblyProduct(
+  "ElectricEngineUnit", "電気エンジンユニット", 20, 1, [[engine_unit, 1], [lubricant, 2], [electronic_circuit, 2]]
+);
+var flying_robot_frame = new AssemblyProduct(
+  "FlyingRobotFrame", "飛行ロボットフレーム", 20, 1, [[electric_engine_unit, 1], [battery, 2], [steel_plate, 1], [electronic_circuit, 3]]
+);
+
+// -----------------
+// 電気系
+// -----------------
+var solar_panel = new AssemblyProduct(
+  "SolarPanel", "ソーラーパネル", 10, 1, [[steel_plate, 5], [electronic_circuit, 15], [copper_plate, 5]]
+);
+var basic_accumulator = new AssemblyProduct(
+  "BasicAccumulator", "蓄電池", 10, 1, [[iron_plate, 2], [battery, 5]]
+);
+
+// -----------------
+// モジュール
+// -----------------
+var speed_module1 = new AssemblyProduct(
+  "SpeedModule1", "生産速度モジュール1", 15, 1, [[electronic_circuit, 5], [advanced_circuit, 5]]
+);
+var speed_module2 = new AssemblyProduct(
+  "SpeedModule2", "生産速度モジュール2", 30, 1, [[speed_module1, 4], [advanced_circuit, 5], [processing_unit, 5]]
+);
+var speed_module3 = new AssemblyProduct(
+  "SpeedModule3", "生産速度モジュール3", 60, 1, [[speed_module2, 4], [advanced_circuit, 5], [processing_unit, 5]]
+);
+var productivity_module1 = new AssemblyProduct(
+  "ProductivityModule1", "生産力モジュール1", 15, 1, [[electronic_circuit, 5], [advanced_circuit, 5]]
+);
+var productivity_module2 = new AssemblyProduct(
+  "ProductivityModule2", "生産力モジュール2", 30, 1, [[productivity_module1, 4], [advanced_circuit, 5], [processing_unit, 5]]
+);
+var productivity_module3 = new AssemblyProduct(
+  "ProductivityModule3", "生産力モジュール3", 60, 1, [[productivity_module2, 5], [advanced_circuit, 5], [processing_unit, 5]]
+);
+var effectivity_module1 = new AssemblyProduct(
+  "EffectivityModule1", "エネルギー効率モジュール1", 15, 1, [[electronic_circuit, 5], [advanced_circuit, 5]]
+);
+var effectivity_module2 = new AssemblyProduct(
+  "EffectivityModule2", "エネルギー効率モジュール2", 30, 1, [[effectivity_module1, 4], [advanced_circuit, 5], [processing_unit, 5]]
+);
+var effectivity_module3 = new AssemblyProduct(
+  "EffectivityModule3", "エネルギー効率モジュール3", 60, 1, [[effectivity_module2, 5], [advanced_circuit, 5], [processing_unit, 5]]
+);
+
+// -----------------
+// 戦闘系
+// -----------------
+var stone_wall = new AssemblyProduct(
+  "StoneWall", "石壁", 0.5, 1, [[stone_brick, 5]]
+);
+var basic_bullet_magazine = new AssemblyProduct(
+  "BasicBulletMagazine", "通常弾薬", 2, 1, [[iron_plate, 2]]
+);
+var piercing_bullet_magazine = new AssemblyProduct(
+  "PiercingBulletMagazine", "貫通弾薬", 3, 1, [[copper_plate, 5], [steel_plate, 1]]
+);
+var granade = new AssemblyProduct(
+  "Granade", "グレネード", 8, 1, [[coal, 10], [iron_plate, 5]]
+);
+var gun_turret = new AssemblyProduct(
+  "GunTurret", "ガンタレット", 8, 1, [[steel_plate, 20], [copper_plate, 10], [iron_gear_wheel, 10]]
+);
+var laser_turret = new AssemblyProduct(
+  "LaserTurret", "レーザータレット", 20, 1, [[steel_plate, 20], [electronic_circuit, 20], [battery, 12]]
+);
+var rocket = new AssemblyProduct(
+  "Rocket", "ロケット弾", 8, 1, [[electronic_circuit, 1], [explosives, 2], [iron_plate, 2]]
+);
+var power_armor_mk2 = new AssemblyProduct(
+  "PowerArmorMK2", "パワーアーマーMK2", 25, 1, [[effectivity_module3, 5], [speed_module3, 5], [processing_unit, 200], [steel_plate, 50]]
+);
+// -----------------
+// ロケット関係
+// -----------------
+var solid_fuel = new ChemicalProduct(
+  "SolidFuel", "固形燃料", 3, 1, [[light_oil, 1]]
+);
+var rocket_fuel = new AssemblyProduct(
+  "RocketFuel", "ロケット燃料", 30, 1, [[solid_fuel, 10]]
+);
+var rocket_control_unit = new AssemblyProduct(
+  "RocketControlUnit", "ロケット制御ユニット", 30, 1, [[processing_unit, 1], [speed_module1, 1]]
+);
+var low_density_structure = new AssemblyProduct(
+  "LowDensityStructure", "断熱材", 30, 1, [[steel_plate, 10], [copper_plate, 5], [plastic_bar, 5]]
+);
+
+// -----------------
+// 組立機
+// -----------------
+var assembling_machine_1 = new AssemblyProduct(
+  "AssemblingMachine1", "組立機1", 0.5, 1, [[electronic_circuit, 3], [iron_gear_wheel, 5], [iron_plate, 9]]
+);
+var assembling_machine_2 = new AssemblyProduct(
+  "AssemblingMachine2", "組立機2", 0.5, 1, [[electronic_circuit, 3], [iron_gear_wheel, 5], [iron_plate, 9], [assembling_machine_1, 1]]
+);
+var assembling_machine_3 = new AssemblyProduct(
+  "AssemblingMachine3", "組立機3", 0.5, 1, [[assembling_machine_2, 2], [speed_module1, 4]]
+);
+
+// -----------------
+// サイエンスパック
+// -----------------
+var science_pack_1 = new AssemblyProduct(
+  "SciencePack1", "サイエンスパック1", 5, 1, [[copper_plate, 1], [iron_gear_wheel, 1]]
+);
+var science_pack_2 = new AssemblyProduct(
+  "SciencePack2", "サイエンスパック2", 6, 1, [[basic_transport_belt, 1], [basic_inserter, 1]]
+);
+var science_pack_3 = new AssemblyProduct(
+  "SciencePack3", "サイエンスパック3", 12, 1, [[advanced_circuit, 1], [engine_unit, 1], [electronic_drill, 1]]
+);
+var millitary_pack = new AssemblyProduct(
+  "MillitaryPack", "軍事サイエンスパック", 10, 1, [[piercing_bullet_magazine, 1], [granade, 1], [gun_turret, 1]]
+);
+
+var products = [
+	iron_ore, copper_ore, coal, stone, iron_plate, copper_plate, stone_brick, steel_plate, concrete, electronic_drill,
+	crude_oil, petroleum_gas, light_oil, heavy_oil, lubricant,
+	iron_gear_wheel, copper_cable, pipe, electronic_circuit, advanced_circuit, processing_unit,
+	basic_transport_belt, fast_transport_belt, express_transport_belt,
+	basic_underground_belt, fast_underground_belt, express_underground_belt,
+	splitter, fast_splitter, express_splitter,
+	basic_inserter, fast_inserter, filter_inserter, stack_inserter, stack_filter_inserter,
+	plastic_bar, sulfur, sulfuric_acid, explosives,
+	battery, engine_unit, electric_engine_unit, flying_robot_frame,
+	solar_panel, basic_accumulator,
+	speed_module1, speed_module2, speed_module3,
+	productivity_module1, productivity_module2, productivity_module3,
+	effectivity_module1, effectivity_module2, effectivity_module3,
+	stone_wall, basic_bullet_magazine, piercing_bullet_magazine, granade, gun_turret, laser_turret, rocket, power_armor_mk2,
+	solid_fuel, rocket_fuel, rocket_control_unit, low_density_structure,
+	assembling_machine_1, assembling_machine_2, assembling_machine_3,
+	science_pack_1, science_pack_2, science_pack_3, millitary_pack,
+];
+
+function getProductByName(name) {
+	return products.filter(function(e) {
+		return e.name == name;
+	})[0];
+}
---
+++ js/view.jsx
@@ -0,0 +1,196 @@
+var counter = 0;
+var Product = React.createClass({
+  getInitialState: function() {
+    var product, cps, imports, language;
+    if (this.props.product === undefined) {
+      product = science_pack_1;
+    } else {
+      product = getProductByName(decodeURI(this.props.product));
+    }
+    if (this.props.cps === undefined) {
+      cps = 0.5;
+    } else {
+      cps = Number(this.props.cps);
+    }
+    if (this.props.imports === undefined) {
+      imports = []
+    } else {
+      imports = decodeURI(this.props.imports).split('_');
+    }
+    if (this.props.language === undefined) {
+      language = 'Japanese'
+    } else {
+      language = this.props.language;
+    }
+    return {
+      product : product,
+      cps     : cps,
+      imports : imports,
+      language: language
+    };
+  },
+  renderProductOptions: function() {
+    var thiz = this;
+    return products.map(function(p) {
+      return(
+        <option value={p.name}>{thiz.state.language=='Japanese' ? p.name_jp : p.name}</option>
+      );
+    });
+  },
+  renderIngredients: function() {
+    var thiz = this;
+    var result = []
+    $.each(this.state.product.require_ingredient_count(this.state.cps, this.state.imports), function(name, icps) {
+      result.push(
+        <Ingredient key={thiz.state.product.name + '_' + name}
+                    product={getProductByName(name)} cps={icps}
+                    addImport={thiz.addImport}
+                    removeImport={thiz.removeImport}
+                    cannotImport={thiz.state.product.name == name}
+                    language={thiz.state.language}
+                    isImport={$.inArray(name, thiz.state.imports) !== -1} />
+      );
+    });
+    return result;
+  },
+  handleLanguageChanged: function(event) {
+    this.setState({language: event.target.value});
+  },
+  handleProductChanged: function(event) {
+    var name = event.target.value;
+    this.setState({product: getProductByName(name), imports: []});
+  },
+  handleCpsChanged: function(event) {
+    if (event.target.value == "") {
+      return;
+    }
+    var val = Number(event.target.value);
+    if (isNaN(val)) {
+      return;
+    }
+    if (val == 0) {
+      return;
+    }
+    this.setState({cps: Number(event.target.value)});
+  },
+  addImport: function(name) {
+    var imports = this.state.imports;
+    imports.push(name);
+    this.setState({imports: $.unique(imports)});
+  },
+  removeImport: function(name) {
+    var imports = this.state.imports.filter(function(i) {
+      return i != name;
+    });
+    this.setState({imports: imports});
+  },
+  text: function(e,j) {
+    return this.state.language == 'Japanese' ? j:e;
+  },
+  render: function() {
+    document.title = this.state.language == 'Japanese' ? this.state.product.name_jp : this.state.product.name;
+    document.location.hash = '#product=' + encodeURI(this.state.product.name) +
+                             '&cps=' + this.state.cps +
+                             '&imports=' + encodeURI(this.state.imports.join('_')) +
+                             '&language=' + this.state.language;
+    var parentonly = (
+      <div>
+        <h3>その他</h3>
+        <ul>
+          <li>各種設備は電気採掘機, 電気炉, 組立機2, 原油精製所(発展的な石油加工) を使用することを想定。</li>
+          <li>固形燃料は軽油から作ることを想定。</li>
+          <li>油井は1つあたり5/sの原油を排出するものとする。</li>
+          <li>水, エイリアンアーティファクトは無限にあるものとしツール上では無視。</li>
+          <li>自分で使うものしかデータ登録してないのでこれが欲しいって人は <a href="https://twitter.com/kojim">@kojim</a> まで。</li>
+          <li>データ古いよとか計算間違ってるよとかの指摘も歓迎</li>
+          <li>iframeタグを使うとこのページを自身のブログ内等に埋め込めます。その際は「その他」以降の情報は表示されなくなるので安心。
+            <pre>
+              例: &lt;iframe width="600" src="http://kojim.github.io/FactorioCalculator.html" frameborder="1"&gt;&lt;/iframe&gt;
+            </pre>
+          </li>
+        </ul>
+        <p>
+          <a href='http://kojim.github.io/Factorio%E8%87%AA%E5%88%86%E7%94%A8%E3%83%A1%E3%83%A2/'>戻る</a><br/>
+        </p>
+      </div>
+    );
+    return (
+      <div className='container'>
+        <select onChange={this.handleLanguageChanged} defaultValue={this.state.language}>
+          <option>Japanese</option>
+          <option>English</option>
+        </select>
+        <h3> {this.text('Product', '生産物')}</h3>
+        <p>
+        <select onChange={this.handleProductChanged} defaultValue={this.state.product.name}>
+          {this.renderProductOptions()}
+        </select>
+        <input type='text' onChange={this.handleCpsChanged} defaultValue={this.state.cps}></input>/s
+        </p>
+        <h3> {this.text('Ingredient', '必要素材')}</h3>
+        <table className='table table-bordered table-striped'>
+          <thead>
+            <tr>
+              <th> {this.text('name', '名前')}</th>
+              <th> <i className="fa fa-arrow-down"></i>/<i className="fa fa-truck"></i> {this.text('producing area', '生産方法')}</th>
+              <th> <i className="fa fa-industry"></i> {this.text('require builders', '必要設備数')}</th>
+              <th> <i className="fa fa-bolt"></i> {this.text('power', '消費電力')}</th>
+              <th> <i className="fa fa-exchange"></i> {this.text('producing/import speed', '生産/搬入速度')}</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.renderIngredients() }
+          </tbody>
+        </table>
+        {window == window.parent? parentonly : ''}
+      </div>
+    );
+  }
+});
+var Ingredient = React.createClass({
+  getInitialState: function() {
+    return {
+      isImport: this.props.isImport,
+      cannotImport: false
+    };
+  },
+  handleNotImport: function() {
+    this.setState({isImport: false});
+    this.props.removeImport(this.props.product.name);
+  },
+  handleImport: function() {
+    this.setState({isImport: true});
+    this.props.addImport(this.props.product.name);
+  },
+  text: function(e,j) {
+    return this.props.language == 'Japanese' ? j:e;
+  },
+  render: function() {
+    var req_builder = this.props.product.require_builder_count(this.props.cps);
+    return (
+      <tr>
+        <td>{this.text(this.props.product.name, this.props.product.name_jp)}</td>
+        <td>
+          <input type='radio' name={this.props.product.name} onChange={this.handleNotImport}
+                              defaultChecked={!this.state.isImport} > {this.text('local', '現地')}</input>
+          <input type='radio' name={this.props.product.name} onChange={this.handleImport}
+                              defaultChecked={this.state.isImport}
+                              disabled={this.props.cannotImport}> {this.text('import', '搬入')}</input>
+        </td>
+        <td>{req_builder.toFixed(1)}</td>
+        <td>{this.state.isImport? 0 :(req_builder*this.props.product.energy_usage).toFixed(0)}kW</td>
+        <td>{this.props.cps.toFixed(3)}/s</td>
+      </tr>
+    );
+  }
+});
+
+var params = {}
+$.each(window.location.hash.substring(1).split('&'), function(i, kv) {
+	kv_array = kv.split('=');
+	params[kv_array[0]] = kv_array[1];
+});
+React.render(
+  <Product product={params['product']} cps={params['cps']} imports={params['imports']} language={params['language']}></Product>,
+  document.getElementById('app-container')
+);
