7679ea443a15b37aae47326906d60b06fe87122f
ykojim <ykojim@gmail.com>
2021-02-21 10:38:39 +0900

    some update

--- js/core.js
+++ js/core.js
@@ -71,14 +71,12 @@ AssemblyProduct.prototype.icon = 'fa fa-industry';
 var Ore = function(name, name_jp) {
 	this.name     = name;
 	this.name_jp  = name_jp;
-	this.req_time = 0.5 / 4; // 採掘対象が採掘範囲に4つあることを想定(採掘対象1つごとに2秒に1つ生産される)
+	this.req_time = 2 / 4; // 採掘対象が採掘範囲に4つあることを想定(採掘対象1つごとに2秒に1つ生産される)
 };
 Ore.prototype = new Product();
 Ore.prototype.ingredients = [];
+Ore.prototype.build_rate = 1;
 Ore.prototype.energy_usage = 420;
-Ore.prototype.require_builder_count =  function(product_per_sec) {
-	return product_per_sec / this.req_time;
-};
 Ore.prototype.icon = 'fas fa-baby-carriage';
 
 // 溶鉱炉で作るもの
